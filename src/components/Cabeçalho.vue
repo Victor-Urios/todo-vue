74c08e465a82ef2a043ad76b242985fc18cd2a4f
Victor-Urios <victorurios@hotmail.com>
2025-06-12 02:03:18 -0300

    Componentes da aplicação

---
+++ src/components/Cabeçalho.vue
@@ -0,0 +1,14 @@
+<!-- Componentes sempre começa com letra maiuscula! -->
+    <script setup>
+        const props = defineProps(['tarefasPendentes'])
+</script> 
+<template>
+    <div class="containter">
+        <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <h1>Minhas tarefas</h1>
+    <p>
+    Você possui {{ props.tarefasPendentes }} tarefas pendentes!
+    </p>
+    </header>
+    </div>
+</template>
